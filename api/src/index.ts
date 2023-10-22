c771f8b9a5ec88bb818dc9f169949385c161c497
M Gilang Januar <mgilangjanuar@gmail.com>
2023-10-22 09:08:58 +0700

    add cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import decrypt from './routes/decrypt'
 import prompt from './routes/prompt'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.post('/prompt/:candidate', prompt)
